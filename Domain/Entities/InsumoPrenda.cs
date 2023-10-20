49647875b8cb08c77e258bbcf2944b5880e74f96
jeangalvis <campusjean2023@gmail.com>
2023-10-20 15:53:13 -0500

    feat(Entities): :sparkles: Entities

--- Domain/Entities/InsumoPrenda.cs
+++ Domain/Entities/InsumoPrenda.cs
@@ -1,8 +1,9 @@
 namespace Domain.Entities;
-    public class InsumoPrenda
+    public class InsumoPrenda : BaseEntity
     {
         public int IdInsumofk { get; set; }
         public Insumo Insumo { get; set; }
         public int IdPrendafk { get; set; }
         public Prenda Prenda { get; set; }
+        public int Cantidad { get; set; }
     }
